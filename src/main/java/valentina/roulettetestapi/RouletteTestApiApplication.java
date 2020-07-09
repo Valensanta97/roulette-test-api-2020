e633ccdd738fd728bb8e1da94f5973fe1cabee23
valentina Santa <valen970211@gmail.com>
2020-07-08 19:40:32 -0500

    Creates the structure of the Roulette API

---
+++ src/main/java/valentina/roulettetestapi/RouletteTestApiApplication.java
@@ -0,0 +1,13 @@
+package valentina.roulettetestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouletteTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouletteTestApiApplication.class, args);
+	}
+
+}
